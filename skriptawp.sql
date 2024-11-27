cc14b2f7fc2b939715b9bdc8156244f5f1378c18
bcacic <cacicbruno@gmail.com>
2024-11-27 20:19:11 +0100

    kreiranje tablice

--- skriptawp.sql
+++ skriptawp.sql
@@ -8,6 +8,11 @@ go
 use edunovawp7;
 --use edunovawp7;
 
-create table smjer(
-naziv varchar(50)
-);
+create table smjerovi(
+sifra int,
+naziv varchar(50),
+trajanje int, 
+cijena decimal(18,2),
+vaucer bit,
+izvodiseod datetime
+); 

cb9435b6ebdc8cf9ebd0386541cdc0b096f8add5
bcacic <cacicbruno@gmail.com>
2024-11-27 19:19:47 +0100

    osnovne naredbe za dorp ako postoji i kreiranje baze

--- skriptawp.sql
+++ skriptawp.sql
@@ -1,5 +1,11 @@
 ﻿--create database edunovawp7;
-
+use master;
+go
+drop database if exists edunovawp7;
+go
+create database edunovawp7
+go
+use edunovawp7;
 --use edunovawp7;
 
 create table smjer(

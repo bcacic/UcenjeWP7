62dbb6a6187b51a904f2d0383b00fe4e965c10be
bcacic <cacicbruno@gmail.com>
2024-12-02 20:59:18 +0100

    nogomet

---
+++ nogomet.sql
@@ -0,0 +1,25 @@
+﻿use master;
+go
+drop database if exists nogomet;
+go
+create database nogomet
+go
+use nogomet;
+go
+create table klub(
+sifra int not null primary key identity(1,1),
+naziv varchar(50) not null,
+osnovan datetime not null,
+stadion varchar(50) not null,
+predsjednik varchar(50) not null,
+drzava varchar(50) not null,
+liga varchar(50)not null
+liga int
+  );
+
+  create table
+
+
+
+use nogomet;
+
